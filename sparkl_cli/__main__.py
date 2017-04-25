31abf0b4ae196c95a7aeec07db3b26a448fd15f0
jacobythwaites <jacoby@sparkl.com>
2017-04-25 10:24:22 +0100

    Corrected lint warnings

--- sparkl_cli/__main__.py
+++ sparkl_cli/__main__.py
@@ -1,7 +1,8 @@
-# Copyright (c) 2017 SPARKL Limited. All Rights Reserved.
-# Author <jacoby@sparkl.com> Jacoby Thwaites.
-# Package main module delegates to main implementation module.
-import sys
+"""
+Copyright (c) 2017 SPARKL Limited. All Rights Reserved.
+Author <jacoby@sparkl.com> Jacoby Thwaites.
+Package main module delegates to main implementation module.
+"""
 
 from . import main
 
